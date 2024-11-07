329310c849404270c013de747d1825698cb51ed3
Arthur Freitas <157178211+arthurfreii@users.noreply.github.com>
2024-11-07 19:38:32 -0300

    Ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { dark } from "@clerk/themes";
 
-const Home = () => {
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return (
-  <Button>Hello word</Button>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName appearance={{
+        baseTheme: dark,
+      }}/>
+    </div>
   );
 };
 
